25362badd364bab70e026fd9290e0d798e22e279
Edajvee <dechols@fubo.tv>
2023-09-24 15:34:25 -0500

    uninstall jest

--- client-booking-app/script/page-operations/progress-bar.js
+++ client-booking-app/script/page-operations/progress-bar.js
@@ -1,39 +1,16 @@
 import { page } from "../component-elements.js"
 
 const arrayOfBars = page.navBar.elements.allProgressionBars
-const currentBarHighlighted = arrayOfBars[i].classList === 'highlighted-bar'
-const nextBarHighlighted = arrayOfBars[i + 1].classList === 'highlighted-bar'
 const progressText = page.navBar.textArea
 
-function lastHighlightedBar() {
-    for (let i = 0; i < arrayOfBars.length - 1; i++) {
-        if (currentBarHighlighted && !nextBarHighlighted) {
-
-        }
-    }
+export function test() {
+    return 2;
 }
 
-export function increaseBarIcon() {
-    
-    for (let i = 0; i < arrayOfBars.length - 1; i++) {
-        if (currentBarHighlighted && !nextBarHighlighted) {
+export function lastHighlightedBar() {
 
-        }
-    }
-}
-
-export function decreaseBarIcon() {
-}
-
-export function increaseProgressText() {
-}
-
-export function decreaseProgressText() {
 }
 
 export default {
-    increaseBarIcon,
-    decreaseBarIcon,
-    increaseProgressText,
-    decreaseProgressText
+    lastHighlightedBar
 }
--- client-booking-app/script/state-machine.js
+++ client-booking-app/script/state-machine.js
@@ -1,60 +1,148 @@
 import { stateEnum } from "./state-enum.js"
+import homePage from "./page-operations/home-page.js"
+import setBookingPage from "./page-operations/set-booking-page.js";
+import progressBar from "./page-operations/progress-bar.js";
 
 export const machine = {
     state: stateEnum.HOME,
     transitions: {
         'home': {
-            pick_service: function(payload) {
-                if (payload.action === 'pick_service') {
-                    console.log("this is true");
-                    this.changeState(stateEnum.PICK_SERVICE);
-                    console.log("State is now", this.state);
+            pick_service: function (action) {
+                if (action.type === 'pick_service') {
+                    homePage.closeHomePage()
+                    setBookingPage.openSetBookingPage()
+                    //progressBar.increaseBarIcon()
+                    this.changeState("pick_service")
+                } else {
+                    console.log('this is the else statement')
                 }
-            },
-            navigation: function(payload) {
-                if (payload.action === 'price')
-                this.changeState(stateEnum.NAV_PRICE)
-                //navigate to price page in nav
             }
         },
-        NAV_PRICE: {
+        'price_navigation': {
 
         },
-        BOOKING_POLICY: {
+        'setBooking': {
 
         },
-        ABOUT_ME: {
+        'about_me': {
 
         },
-        PICK_SERVICE: {
+        'pick_service': {
 
         },
-        KYC: {
+        'know_your_cutsomer': {
 
         },
-        BOOKING: {
+        'booking': {
 
         },
-        FINALIZE: {
+        'finalize': {
 
         }
     },
     dispatch(actionName, ...payload) {
-        const actions = this.transitions[this.state];
+                const actions = this.transitions[this.state];
+                const action = this.transitions[this.state][actionName];
         
-        if (actions) {
-            const action = actions[actionName];
-            if (typeof action === 'function') {
-                action.apply(machine, ...payload);
-                console.log(`Action '${actionName}' dispatched.`);
-            } else {
-                console.error(`Invalid action '${actionName}' for state '${this.state}'.`);
+                if (action) {
+                    action.apply(machine, ...payload);
+                } else {
+                    console.log("action is not valid for current state");
+                }
+            },
+            changeState(newState) {
+                this.state = newState;
             }
-        } else {
-            console.error(`No valid actions defined for state '${this.state}'.`);
-        }
-    },
-    changeState(newState) {
-        this.state = newState;
-    }
 };
+
+// export const machine =  {
+//     state: "SOBER",
+//     transitions: {
+//         SOBER: {
+//             drink: function (beverage) {
+//                 console.log('current state', this.state);
+//                 console.log('\tdrinking ', beverage.type);
+//                 if (beverage.type === 'alcohol') {
+//                     console.log('\tAdios inhibitions!')
+//                     this.changeState("DRUNK")
+//                 } else {
+//                     console.log('\tThat quenched my thirst!')
+//                 }
+//             }
+//         },
+//         DRUNK: {
+//             drink: function (beverage) {
+//                 console.log('current state', this.state);
+//                 console.log('\tdrinking ', beverage.type);
+//                 if (beverage.type === 'alcohol') {
+//                     console.log('\tAdios pants!')
+//                     this.changeState("REALLYDRUNK")
+//                 } else {
+//                     console.log("\tI said what?")
+//                     this.changeState("SOBER")
+//                 }
+//             }
+//         },
+//         REALLYDRUNK: {
+//             drink: function (beverage) {
+//                 console.log('current state', this.state);
+//                 console.log('\tdrinking ', beverage.type);
+//                 if (beverage.type === 'alcohol') {
+//                     let dice = Math.floor(Math.random() * 2);  // 0 or 1
+//                     if (dice) {
+//                         this.dispatch("throwup", {});
+//                     } else {
+//                         this.dispatch("passout", {});
+//                     }
+//                 } else {
+//                     this.changeState("DRUNK")
+//                 }
+//             },
+//             passout: function() {
+//                 console.log('\tPassing out. zzzzzzzzzzz')
+//                 this.changeState('ASLEEP')
+//             },
+//             throwup: function() {
+//                 console.log('\tBlaaaaaaaaaaaa....When did I eat that?');
+//                 this.dispatch('passout', {});
+//             }
+//         },
+//         ASLEEP: {
+//             wake: function() {
+//                 console.log('current state', this.state);
+//                 console.log('\tWaking up');
+//                 this.changeState("HUNGOVER")
+//             }
+//         },
+//         HUNGOVER: {
+//             openEyes: function() {
+//                 console.log("current state", this.state);
+//                 console.log("\tTurn off the sun please");
+//             },
+//             drink: function(beverage) {
+//                 console.log('current state', this.state);
+//                 console.log('\tdrinking ', beverage.type);
+//                 if (beverage.type === 'alcohol') {
+//                     console.log('\tIs it never again yet?');
+//                     this.changeState('DRUNK')
+//                 } else {
+//                     console.log('\tNever again');
+//                     this.changeState('SOBER')
+//                 }
+//             }
+//         }
+//     },
+//     dispatch(actionName, ...payload) {
+//         const actions = this.transitions[this.state];
+//         const action = this.transitions[this.state][actionName];
+
+//         if (action) {
+//             action.apply(machine, ...payload);
+//         } else {
+//             //action is not valit for current state
+//         }
+//     },
+//     changeState(newState) {
+//         this.state = newState;
+//     }
+// };
