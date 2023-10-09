54404fa299542b2bd44795c23ff45226f4da0c0c
Dajve Echols <edajve@gmail.com>
2023-10-08 21:36:07 -0500

    MAKE FILLER PROPORTIONAL TO SLIDER

--- client-booking-app/script/page-operations/set-day-time.js
+++ client-booking-app/script/page-operations/set-day-time.js
@@ -4,7 +4,7 @@ import database from "../db/database.js"
 const title = page.setBookingPage.subPage.dayUI.elements.titleDate
 
 function showBookingTimeTitle() {
-    title.innerHTML = `${database.database.booking.month}, ${database.database.booking.date} ${database.database.booking.day}`
+    title.innerHTML = `${database.database.booking.month}, ${database.database.booking.date}`
 }
 
 function onTimeSlotClick(slot) {
@@ -19,6 +19,8 @@ function onTimeSlotClick(slot) {
 
     clearAnyPrevChosenTime()
     const target = targetElement.parentNode.id
+    const guiTime = targetElement.parentNode.getAttribute("gui-date")
+    title.innerHTML = `${database.database.booking.month}, ${database.database.booking.date} @${guiTime}`
     targetElement = document.getElementById(`specific-time-${target}`)
     targetElement.parentNode.insertAdjacentElement("afterend", createFillerElement())
 }
@@ -51,11 +53,11 @@ function createFillerElement() {
 }
 
 function onSliderMove() {
-    const value = page.setBookingPage.subPage.dayUI.elements.slider.value
+    let value = page.setBookingPage.subPage.dayUI.elements.slider.value
     page.setBookingPage.subPage.dayUI.elements.sliderText.innerHTML = `Length: ${value} minutes`
 
     const filler = document.getElementById('filler')
-    if (filler) filler.style.height = `${value - 10}px`
+    if (filler) filler.style.height = `${fillerAnimation(value)}px`
 }
 
 page.setBookingPage.subPage.dayUI.elements.timeSlots.forEach(slot => { slot.addEventListener('click', slot => onTimeSlotClick(slot)) })
@@ -64,3 +66,18 @@ page.setBookingPage.subPage.dayUI.elements.slider.addEventListener('input', onSl
 export default {
     showBookingTimeTitle
 }
+
+function fillerAnimation(slideVal) {
+    const SLIDER_MIN = 0
+    const SLIDER_MAX = 240
+    slideVal = Math.max(SLIDER_MIN, Math.min(SLIDER_MAX, slideVal));
+    const pixels = (slideVal / SLIDER_MAX) * 199
+    return pixels;
+}
+//write a method that grows the filler in day calendar in proportion to the scroll bar
+
+// min 0 on scroll bar
+// max 240 on scroll bar
+// default scroll bar minutes is 60 minutes
+// 240 minutes is 4 hours
+// each hour is 48 pixels on the cal
